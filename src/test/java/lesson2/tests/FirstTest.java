96a7f6a1bdd9ffd230066d8af2f9345f57430d27
Alexey Kosovskiy <al.kosovsky@gmail.com>
2022-10-02 12:48:14 +0300

    Занятие 2. ДЗ

---
+++ src/test/java/lesson2/tests/FirstTest.java
@@ -0,0 +1,39 @@
+package lesson2.tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setup() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:/Java/GitHub/Projects/MobileATCourse/apks/wiki.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest() {
+        System.out.println("First test run");
+    }
+}
